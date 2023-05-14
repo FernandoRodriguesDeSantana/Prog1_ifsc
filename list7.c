75138c0ef751bb6d4b7f1afad0574350c8d64ae0
Fernando Rodrigues de Santana <fernando.s11@aluno.ifsc.edu.br>
2023-05-14 14:51:50 -0300

    Update list7.c

--- list7.c
+++ list7.c
@@ -21,3 +21,62 @@ int main(){
 
 // ===============================================================================================================
 
+// 2) Faça uma função que receba a data atual (dia, mês e ano em inteiro) e exiba-a na tela no formato textual por extenso. Exemplo: Data: 01/01/2000, Imprimir: 1 de janeiro de 2000.
+
+void verData(int dia, int mes, int ano){
+	
+	switch(mes){
+		case 1: 
+			printf("%d de janeiro de %d", dia, ano);
+			break;
+		case 2: 
+			printf("%d de fevereiro de %d", dia, ano);
+			break;
+		case 3: 
+			printf("%d de março de %d", dia, ano);
+			break;
+		case 4: 
+			printf("%d de abril de %d", dia, ano);
+			break;
+		case 5: 
+			printf("%d de maio de %d", dia, ano);
+			break;
+		case 6: 
+			printf("%d de junho de %d", dia, ano);
+			break;
+		case 7: 
+			printf("%d de julho de %d", dia, ano);
+			break;
+		case 8: 
+			printf("%d de agosto de %d", dia, ano);
+			break;
+		case 9: 
+			printf("%d de setembro de %d", dia, ano);
+			break;
+		case 10: 
+			printf("%d de outubro de %d", dia, ano);
+			break;
+		case 11: 
+			printf("%d de novembro de %d", dia, ano);
+			break;
+		case 12: 
+			printf("%d de dezembro de %d", dia, ano);
+			break;
+		default:
+			printf("Data inválida");
+			break;
+	}
+}
+
+int main(){
+	int dia, mes, ano;
+	
+	printf("Entre com o dia, mes e ano: ");
+	scanf("%d%d%d", &dia, &mes, &ano);
+
+	verData(dia, mes, ano);
+
+	return 0;
+}
+
+// ===============================================================================================================
