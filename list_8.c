6429b8d987a860c4cfbf52b95dd0f347b4b415bc
Fernando Rodrigues de Santana <fernandobr11sp@gmail.com>
2023-06-02 21:13:41 -0300

    Update list_8.c

--- list_8.c
+++ list_8.c
@@ -33,5 +33,49 @@ int main()
     printf("\nPerson adress: %s", informations.adress);
 
     return 0;
-//a
+
+}
+
+//======================================================================================================================================
+
+//Construa uma estrutura aluno com nome, número de matrıcula e curso. Leia do usuário a 
+//informação de 5 alunos, armazene em vetor dessa estrutura e imprima os dados na tela
+
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+struct student{
+	char name[50];
+	int code;
+	char course[50];
+};
+
+int main(){
+	struct student info[5];
+	int i;
+
+	for(i = 0; i < 5; i++){
+		printf("Enter the student name: ");
+		gets(info[i].name);
+		fflush(stdin);
+
+		printf("\nEnter the student code: ");
+		scanf("%d", &info[i].code);
+		fflush(stdin);
+
+		printf("\nEnter the student course: ");
+		gets(info[i].course);
+		fflush(stdin);
+
+		printf("\n\n");
+	}
+
+	for(i = 0; i < 5; i++){
+		printf("\n------------Information of the student------------");
+
+		printf("\nThe student %s with code %d from the course of %s are registered in the system", info[i].name, info[i].code, info[i].course);
+		printf("\n.........................Next.........................\n");
+	}
+	return 0;
 }
