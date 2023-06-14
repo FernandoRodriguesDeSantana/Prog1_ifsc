7dfcb36beefca0fc14d47d35ed8f6f7a734840ad
Fernando Rodrigues de Santana <fernandobr11sp@gmail.com>
2023-06-14 20:14:18 -0300

    Update Class13_list9.c

--- Class13_list9.c
+++ Class13_list9.c
@@ -1,1 +1,5 @@
+/*1. Escreva um programa que declare um inteiro, um real e um char, e ponteiros para in-
+teiro, real, e char. Associe as variaveis aos ponteiros (use &). Modifique os valores de
+cada variavel usando os ponteiros. Imprima os valores das variaveis antes e apos a
+modificacao.*/
 
